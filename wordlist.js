501d4435c068b46a5f268bb6620e84105405a69e
Praveen D R <praveendr07@gmail.com>
2024-07-20 23:39:13 +0530

    Add files via upload

---
+++ wordlist.js
@@ -0,0 +1,63 @@
+
+
+const wordList = [
+    {
+      word: "Guitar",
+      hint: "A musical instrument with strings.",
+    },
+    {
+      word: "school",
+      hint: "Education center.",
+    },
+    {
+      word: "football",
+      hint: "ball game.",
+    },
+    {
+      word: "pizza",
+      hint: "A savory dish consisting of a round, flattened base with toppings.",
+    },
+    {
+      word: "camera",
+      hint: "A device used to capture and record images or videos.",
+    },
+    {
+      word: "coffee",
+      hint: "A popular caffeinated beverage made from roasted coffee beans.",
+    },
+    {
+      word: "dance",
+      hint: "A rhythmic movement of the body often performed to music.",
+    },
+    {
+      word: "astronaut",
+      hint: "A person trained to travel and work in space.",
+    },
+    {
+      word: "waterfall",
+      hint: "A cascade of water falling from a height.",
+    },
+    {
+      word: "piano",
+      hint: "A musical instrument played by pressing keys that cause hammers to strike strings.",
+    },
+    {
+      word: "puzzle",
+      hint: "A game, toy, or problem designed to test ingenuity or knowledge.",
+    },
+    {
+      word: "whisper",
+      hint: "To speak very softly or quietly, often in a secretive manner.",
+    },
+    {
+      word: "secret",
+      hint: "Something kept hidden or unknown to others.",
+    },
+  
+    {
+      word: "vibrant",
+      hint: "Full of energy, brightness, and life.",
+    },
+    
+  ];
+  
